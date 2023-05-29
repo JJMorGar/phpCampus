f5d80afb75f8a039cb6dfaf87a0ff34af6b23119
ccrs11 <ccrs11@hotmail.com>
2023-05-29 17:28:16 -0500

    feat(ejercicios): :sparkles: punto 6

--- index.php
+++ index.php
@@ -1,9 +1,36 @@
 
 <?php
-    /**
-     * punto 5
-     * etiquetas que marcan el inicio y fin del script php
-     */
+/**
+ * punto 5
+ * etiquetas que marcan el inicio y fin del script php
+ */
 
-     echo '<h1>hola mundo cruel</h1>';
+echo '<h1>hola mundo cruel</h1>';
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                   punto 6                                  */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * funciones de salida
+ * mostrar información al usuario cuando finaliza la ejecución de un proceso determinado o cuando finaliza el
+ * proceso una petición realizada por el usuario final en la salida estandar.
+ */
+
+
+//mostrar una varias string
+echo 'texto a imprimir';
+
+// imprimir con formato
+$strSaludo = "mundi"; 
+printf("hola mundo %s ", $strSaludo);
+
+// devuelve el texto formateado NO LO IMPRIME
+$mensaje = sprintf("hola %s", $strSaludo);
+echo $mensaje;
+
+
+
 ?>
